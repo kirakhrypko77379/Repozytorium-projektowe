55c51f6c96ec8e6db9596609355b74c5495cf3cb
kirakhrypko77379 <kirahripko@gmail.com>
2026-04-18 16:24:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -25,3 +25,55 @@ toggleBtn.addEventListener("click", () => {
         skillsSection.style.display = "none";
     }
 });
+
+const contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", function(e) {
+    e.preventDefault(); 
+    
+    let isValid = true;
+
+    const nameRegex = /^[a-zA-Zа-яА-ЯёЁ\s]+$/; 
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+
+    
+    const fields = [
+        { id: 'firstName', errorId: 'firstNameError', label: 'Imię' },
+        { id: 'lastName', errorId: 'lastNameError', label: 'Nazwisko' },
+        { id: 'email', errorId: 'emailError', label: 'E-mail' },
+        { id: 'message', errorId: 'messageError', label: 'Wiadomość' }
+    ];
+
+    fields.forEach(field => {
+        const input = document.getElementById(field.id);
+        const errorSpan = document.getElementById(field.errorId);
+        
+        errorSpan.textContent = "";
+
+        if (!input.value.trim()) {
+            errorSpan.textContent = `Pole ${field.label} jest wymagane.`;
+            isValid = false;
+        } 
+        
+        else if ((field.id === 'firstName' || field.id === 'lastName') && !nameRegex.test(input.value)) {
+            errorSpan.textContent = "To pole nie może zawierać cyfr.";
+            isValid = false;
+        }
+       
+        else if (field.id === 'email' && !emailRegex.test(input.value)) {
+            errorSpan.textContent = "Proszę wpisać poprawny adres e-mail.";
+            isValid = false;
+        }
+    });
+
+    
+    if (isValid) {
+        document.getElementById("successMsg").style.display = "block";
+        contactForm.reset(); 
+        
+        
+        setTimeout(() => {
+            document.getElementById("successMsg").style.display = "none";
+        }, 5000);
+    }
+});
